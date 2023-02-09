e889aaf49f85f9d55d12c84f0ce5a27aa13b7e1e
Nicholas Horton <nicholasjhorton@gmail.com>
2023-02-09 09:20:41 -0500

    See https://github.com/STAT210-S23/Parsons_Paper_Register/issues/8 for a stub.

---
+++ shiny_display/README.md
@@ -0,0 +1,1 @@
+Stub of a Shiny app to display pages (along with other information) for the Parsons Paper Company register.
---
+++ shiny_display/app.R
@@ -0,0 +1,59 @@
+library(png) # For writePNG function
+library(shiny)
+
+# Return a matrix with 2d guassian distribution
+gauss2d <- function(x, y, r = 0.15) {
+  exp(
+    -((x - 0.5)^2 + (y - 0.5)^2) /
+      (2 * r^2)
+  )
+}
+
+
+ui <-
+  fluidPage(
+    titlePanel("Image output"),
+    
+    fluidRow(
+      column(4, wellPanel(
+        radioButtons("picture", "Picture:",
+                     c("chainring", "face"))
+      )),
+      column(4,
+             imageOutput("image2")
+      )
+    )
+  )
+
+  
+server <- function(input, output) {
+  # image2 sends pre-rendered images
+  output$image2 <- renderImage({
+    if (is.null(input$picture))
+      return(NULL)
+    
+    if (input$picture == "face") {
+      return(list(
+        src = "face.png",
+        contentType = "image/png",
+        alt = "Face"
+      ))
+    } else if (input$picture == "chainring") {
+      return(list(
+        src = "chainring.jpg",
+        filetype = "image/jpeg",
+        alt = "This is a chainring"
+      ))
+    }
+    
+  }, deleteFile = FALSE)
+}
+
+
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
